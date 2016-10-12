a4f1dfc9e25085c10622c5378e5e2ae1c49b6a26
Frankie Roberto <frankie@frankieroberto.com>
2016-10-12 16:33:17 +0100

    Make work for real

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,11 @@
 class CollectionsController < ApplicationController
 
 
+  def index
+    @collections = Collection.all
+  end
+
+
   def show
     @collection = Collection.find(params[:id])
   end
