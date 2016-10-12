f5feac91fe240862e119178bffa4f7c12ac65d9a
Frankie Roberto <frankie@frankieroberto.com>
2016-10-12 16:46:03 +0100

    Actual thing counts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  # Same as pluralize helper but with the number formatted using
+  # a delimiter.
+  def pluralize_with_delimiter(count, singular, plural = nil)
+    word = if (count == 1 || count =~ /^1(\.0+)?$/)
+      singular
+    else
+      plural || singular.pluralize
+    end
+
+    "#{number_with_delimiter(count) || 0}&nbsp;#{word}".html_safe
+  end
+
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,13 +2,14 @@ require 'csv'
 
 class Collection
 
-  attr_accessor :id, :institution_id, :name
+  attr_accessor :id, :institution_id, :name, :size_int
 
   def initialize(metadata)
     @metadata = metadata
     @id = metadata['gfs_id']
     @institution_id = metadata['institution']
     @name = metadata['collection']
+    @size_int = metadata['size'].to_s.gsub(',', '').to_i
   end
 
   def [](test)
@@ -21,7 +22,7 @@ class Collection
     data = CSV.read("#{File.dirname(__FILE__)}/../../db/collections.csv", headers: true)
 
     data[2..-1].collect do |row|
-      self.new(row.to_h.merge({'size_int' => row['size'].to_s.gsub(',', '').to_i}))
+      self.new(row.to_h)
     end
 
   end
