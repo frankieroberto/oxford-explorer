3a2fd66337f7a948e23bfbefc725ed253b89eb09
George Oates <glo@Georges-MBP.home>
2016-10-12 12:30:47 +0100

    getting started on home page layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :collections, only: [:index, :show]
 
-  resources :items, only: [:show]
+  resources :things, only: [:show]
 
   resources :institutions, only: [:show]
 
