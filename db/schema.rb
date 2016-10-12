a4c73ea9997c2e406eeb4016d700014609208827
Frankie Roberto <frankie@frankieroberto.com>
2016-10-12 12:32:39 +0100

    enable unaccent

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 20161012113149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "unaccent"
   enable_extension "hstore"
+  enable_extension "unaccent"
 
   create_table "collections", force: :cascade do |t|
     t.text "name",               null: false
