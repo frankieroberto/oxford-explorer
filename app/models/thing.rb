ac649262c7dda15e8119d29ca1b582fb01495f61
Frankie Roberto <frankie@frankieroberto.com>
2016-10-19 11:48:04 +0100

    accessor for department

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -2,11 +2,12 @@ class Thing
 
   ES_CLIENT = Elasticsearch::Client.new url: ENV.fetch('ELASTICSEARCH_URL'), log: false
 
-  attr_reader :metadata, :title, :id
+  attr_reader :metadata, :title, :id, :description
 
   def initialize(metadata)
     @metadata = metadata
     @title = Array(metadata['gfs_title']).first || '(untitled)'
+    @description = Array(metadata['gfs_description']).first
     @id = metadata['gfs_id']
   end
 
