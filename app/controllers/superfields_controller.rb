e611ddd97ae22a82402be2e0b883eceb10c1e558
Frankie Roberto <frankie@frankieroberto.com>
2016-10-19 16:15:36 +0100

    Merge collections from spreadsheet with item data

--- app/controllers/superfields_controller.rb
+++ app/controllers/superfields_controller.rb
@@ -16,6 +16,33 @@ class SuperfieldsController < ApplicationController
     @things_by_subjects = thing_query.things_by_subjects
     @things_by_institution = thing_query.things_by_institution
 
+
+    additional_collections = []
+
+    if @superfield == 'gfs_subject.raw'
+
+      additional_collections += additional_collections =  Collection.all.select {|collection| collection.subjects.include?(@id) }
+
+    elsif @superfield == 'gfs_item_type.raw'
+
+      additional_collections += Collection.all.select {|collection| collection.types_of_things.collect(&:name).include?(@id) }
+
+    elsif @superfield == 'gfs_author.raw'
+
+      additional_collections += Collection.all.select {|collection| collection.people.include?(@id) }
+
+    end
+
+    additional_collections.each do |collection|
+
+      if !@things_in_collections.collect(&:id).include?(collection.id)
+        @things_in_collections << OpenStruct.new(collection: collection, count: nil)
+      end
+
+    end
+
+
+
     @min_pubyear = thing_query.min_pubyear&.to_i
     @max_pubyear = thing_query.max_pubyear&.to_i
 
