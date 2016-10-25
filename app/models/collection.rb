bc09f4de6ef4e77bd236dda4413a8a91cd977550
Frankie Roberto <frankie@frankieroberto.com>
2016-10-25 11:39:59 +0100

    Merge computed data with collections data
    Fixes #44

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class Collection
 
-  attr_accessor :id, :institution_id, :name, :size_int, :subjects, :places,
+  attr_accessor :id, :institution_id, :name, :size_int, :subjects, :places, :metadata,
     :people, :dates, :digitized_metadata_size_int, :digitized_size_int, :department, :academic_departments, :divisions
 
   def initialize(metadata)
@@ -11,7 +11,6 @@ class Collection
     @institution_id = metadata['institution']
     @department = metadata['department']
     @name = metadata['collection']
-    @size_int = metadata['size'].to_s.gsub(',', '').to_i
     @digitized_metadata_size_int = metadata['published_digital_records'].to_s.gsub(',', '').to_i
     @digitized_size_int = metadata['how_many_digitized_versions'].to_s.gsub(',', '').to_i
     @subjects = metadata['subjects'].to_s.split(/[\,;]/).collect(&:strip).reject(&:blank?).collect(&:downcase)
@@ -58,8 +57,35 @@ class Collection
     end
   end
 
+  def size_int
+    @size_int ||= begin
+
+      metadata_size_int = metadata['size'].to_s.gsub(',', '').to_i
+      computed_data_size = computed_data['count'].to_i
+
+      [metadata_size_int, computed_data_size].max
+    end
+  end
+
+  private
+
+  def computed_data
+    Collection.all_computed_data.detect {|collection| collection['item'] == id } || {}
+  end
+
+  def self.all_computed_data
+
+    @all_computed_data ||= begin
+      CSV.read("#{Rails.root.join("db", "collections_computed.csv").to_s}", headers: true).collect do |row|
+        row.to_h
+      end
+    end
+
+  end
+
   def self.all
     @all ||= begin
+
       data = CSV.read("#{Rails.root.join("db", "collections.csv").to_s}", headers: true)
 
       data[2..-1].collect do |row|
