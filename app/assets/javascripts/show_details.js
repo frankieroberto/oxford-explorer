cbb0e69f8cd3dc9ad6443159afa0f78b2cf4b6cf
Tom Armitage <tom@infovore.org>
2016-10-26 17:25:58 +0100

    Make show more toggle to hide when clicked.

--- app/assets/javascripts/show_details.js
+++ app/assets/javascripts/show_details.js
@@ -1,7 +1,4 @@
-
-
 function addShowHideDetails() {
-
   var elements = document.getElementsByClassName('js-show-details');
 
   for (var i = elements.length - 1; i >= 0; i--) {
@@ -9,10 +6,16 @@ function addShowHideDetails() {
     var show_hide_links = elements[i].getElementsByClassName('js-show');
     var details_elements = elements[i].getElementsByClassName('js-details')
 
+    var show_hide_links = elements[i].getElementsByClassName('js-show');
+
     for (var j = show_hide_links.length - 1; j >= 0; j--) {
-
-      show_hide_links[i].addEventListener('click', function() {
-
+      show_hide_links[j].addEventListener('click', function() {
+        console.log(this);
+        if(this.innerText.toLowerCase().match('Show more')) {
+          this.innerText = "Hide";
+        } else {
+          this.innerText = "Show more";
+        }
 
         for (var k = details_elements.length - 1; k >= 0; k--) {
           details_elements[k].classList.toggle('shown');
